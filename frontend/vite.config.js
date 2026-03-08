cb5017362ba8ee5f48230a6d946df157f8d7e2b7
root <root@DESKTOP-P43289U.localdomain>
2026-03-08 15:58:41 -0500

    actualizacion port con claudflare

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 5173,
+    allowedHosts: ['.trycloudflare.com'],
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'http://backend:3000',
         changeOrigin: true
       }
     }
